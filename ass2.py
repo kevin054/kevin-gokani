9932ab6aff78306bfcb02dfac418881909357384
kevin054 <150303105054@paruluniversity.ac.in>
2017-02-11 15:15:16 +0530

    Add files via upload

---
+++ ass2.py
@@ -0,0 +1,7 @@
+radius=int(raw_input("enter the radius"))
+area=3.14*radius**2
+cir=2*3.14*radius
+print "area"
+print area
+print "circumfrence"
+print cir
---
+++ ass3.py
@@ -0,0 +1,7 @@
+cm=int(raw_input("enter data in centimeter"))
+km=float(cm/100000)
+m=float(cm/100)
+print "kilometer"
+print km
+print "meter"
+print m
